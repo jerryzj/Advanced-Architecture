1c9fa5e57878a5b82cb012c642c9dfc1980621fb
Jerry ZJ <jerry0715no14@gmail.com>
2017-12-28 19:39:09 +0800

    Add parse script

---
+++ Project3/parse_bimod.py
@@ -0,0 +1,16 @@
+import glob
+
+FILE_OUT = "summary.txt"
+file_out = open(FILE_OUT, 'w')
+for FILE_IN in glob.glob("*.stderr"):
+	file_out.write(FILE_IN + '\n')
+	file_in = open(FILE_IN)
+	for line in file_in:
+		if "bpred_bimod.bpred_dir_rate" in line:
+			file_out.write(line)
+		if "sim_IPC" in line:
+			file_out.write(line)
+	file_out.write("\n")
+	file_in.close()
+
+file_out.close()
