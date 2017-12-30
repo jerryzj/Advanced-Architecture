9737b258b3051fcda1762f137f45c60ecde1d356
Jerry ZJ <jerry0715no14@gmail.com>
2017-12-30 20:27:14 +0800

    update automatic script

--- Project3/ee6455_p3/Auto_build1225.sh
+++ Project3/ee6455_p3/Auto_build1225.sh
@@ -1,12 +1,103 @@
 #!/bin/bash -uxe
 
+# 1. Run in bimod mode
 # Set config file, "bimod" prediction 
-sed -i '15s/.*/-bpred                       bimod/' default.baseline.cfg
-sed -i '16s/.*/-bpred:bimod 1024/' default.baseline.cfg
+sed -i '16s/.*/-bpred                       bimod/' default.baseline.cfg
+sed -i '17s/.*/-bpred:bimod 4096/' default.baseline.cfg
+# Set branch delay
+sed -i '31s/.*/-fetch:mplat 3/' default.baseline.cfg
 # Run SPEC2000 benchmarks
-# make -f SPEC2000.make all
+make -f SPEC2000.make all
+# Copy parse script and run
+sudo cp parse_bimod.py ./results
+cd results
+sudo python parse_bimod.py
+cd ..
 # Copy test results
-# sudo cp -r ./results ~/Results/bimod
+sudo cp -r ./results ~/Results/bimod_3
 # Clean
-# rm -rf ./results
-# mkdir results
+rm -rf ./results
+mkdir results
+
+# Set branch delay
+sed -i '31s/.*/-fetch:mplat 9/' default.baseline.cfg
+# Run SPEC2000 benchmarks
+make -f SPEC2000.make all
+# Copy parse script and run
+sudo cp parse_bimod.py ./results
+cd results
+sudo python parse_bimod.py
+cd ..
+# Copy test results
+sudo cp -r ./results ~/Results/bimod_9
+# Clean
+rm -rf ./results
+mkdir results
+
+# 2. Run in 2level mode
+# Set config file, "bimod" prediction 
+sed -i '16s/.*/-bpred                       2lev/' default.baseline.cfg
+sed -i '17s/.*/-bpred:2lev 1 4096 12 0 /' default.baseline.cfg
+# Set branch delay
+sed -i '31s/.*/-fetch:mplat 3/' default.baseline.cfg
+# Run SPEC2000 benchmarks
+make -f SPEC2000.make all -j8
+# Copy parse script and run
+sudo cp parse_2lev.py ./results
+cd results
+sudo python parse_2lev.py
+cd ..
+# Copy test results
+sudo cp -r ./results ~/Results/2lev_3
+# Clean
+rm -rf ./results
+mkdir results
+
+# Set branch delay
+sed -i '31s/.*/-fetch:mplat 9/' default.baseline.cfg
+# Run SPEC2000 benchmarks
+make -f SPEC2000.make all -j8
+# Copy parse script and run
+sudo cp parse_2lev.py ./results
+cd results
+sudo python parse_2lev.py
+cd ..
+# Copy test results
+sudo cp -r ./results ~/Results/2lev_9
+# Clean
+rm -rf ./results
+mkdir results
+
+# 3. Run in Alpha21264 mode
+# Set config file, "bimod" prediction 
+sed -i '16s/.*/-bpred                      Comb/' default.baseline.cfg
+sed -i '17s/.*/#-bpred:2lev 1 4096 12 0 /' default.baseline.cfg
+# Set branch delay
+sed -i '31s/.*/-fetch:mplat 3/' default.baseline.cfg
+# Run SPEC2000 benchmarks
+make -f SPEC2000.make all -j8
+# Copy parse script and run
+sudo cp parse_comb.py ./results
+cd results
+sudo python parse_comb.py
+cd ..
+# Copy test results
+sudo cp -r ./results ~/Results/comb_3
+# Clean
+rm -rf ./results
+mkdir results
+
+# Set branch delay
+sed -i '31s/.*/-fetch:mplat 9/' default.baseline.cfg
+# Run SPEC2000 benchmarks
+make -f SPEC2000.make all -j8
+# Copy parse script and run
+sudo cp parse_comb.py ./results
+cd results
+sudo python parse_comb.py
+cd ..
+# Copy test results
+sudo cp -r ./results ~/Results/comb_9
+# Clean
+rm -rf ./results
+mkdir results
